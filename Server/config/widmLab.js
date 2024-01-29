3d65a070f047e9b37e064f66cae4f34f983754f4
黃懷萱 <56072039+Chrouos@users.noreply.github.com>
2024-01-29 19:58:00 +0800

    隱藏資訊

--- Server/config/widmLab.js
+++ Server/config/widmLab.js
@@ -1,20 +1,18 @@
 module.exports = {
-    GPT_KEY: "ENC(9y4hkc9RPdIan7cCtvkjwkoo0nkpZiIgBhyUni0sEZ/W01g54w/InGEZqaE6rZjyri/R78hl/mEEBcP2qto6UlZXfUjFoFw5JLHxADWznBs=)", // encryption
+    GPT_KEY: "ENC(......)", // encryption
 
     PORT: 8280,
-    HOSTNAME: "140.115.54.58",
+    HOSTNAME: "localhost",
 
     CHROMA_NAME: "Carash_db",
-    CHROMA_HOSTNAME: "140.115.54.58",
+    CHROMA_HOSTNAME: "140.115.54.xx",
 
     MONGODB_PROTOCOL: 'mongodb',
-    MONGODB_USERNAME: 'ENC(MYqrElfgXgv7Ctrm2gltg8uFOvpdUx+VSc9hIhKQxBg=)',
-    MONGODB_PASSWORD: 'ENC(q4+52DEbdx5/yxg/JKOZj2TXw3zqhR4oeDIFWfW89IE=)',
-    MONGODB_HOST: '140.115.54.58',
+    MONGODB_USERNAME: 'ENC(...)',
+    MONGODB_PASSWORD: 'ENC((...)',
+    MONGODB_HOST: '140.115.54.xx',
     MONGODB_PORT: '27027',
     MONGODB_DBNAME: 'CCG',
     MONGODB_AUTHSOURCE: 'CCG'
 
 }
-
-
