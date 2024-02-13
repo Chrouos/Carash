5baf8b0f250d2d7bc164071bb04a2a41f1d4c1eb
Huai <chrbezz0487@gmail.com>
2024-02-13 08:24:19 +0800

    統一管理 Prompt

--- Server/controllers/AccidentDetailsController.js
+++ Server/controllers/AccidentDetailsController.js
@@ -72,11 +72,7 @@ exports.retrievalContent = async (req, res) => {
         if (Object.values(requestData.incidentJson["車禍發生事故"]).filter(value => value !== "").length == 0) {
 
             // + 初始模組
-            firstPrompt = `你現在是一件交通諮詢的專家，現在有一件交通[事故敘述]，請你將資訊歸納成如下的[事件儲存Json格式]，如果沒有資料請保持欄位空白。\n` +
-            `\n[事件儲存Json格式]:\n ${JSON.stringify(requestData.incidentJson["車禍發生事故"])}` +
-            `\n[事故敘述]:\n ${requestData.userDescription}` +
-            `\n[Json]:`;
-
+            const { firstPrompt } = require("./data/prompt")
             const firstMessages = [ { "role": "system", "content": firstPrompt } ]
             const gptResponse = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo-1106",
@@ -109,13 +105,7 @@ exports.retrievalContent = async (req, res) => {
         else {
 
             // + 擷取模組
-            tidyPrompt = 
-            `你是一位事件擷取機器人，現在有一[問題]與該[問題回覆]和一個[Json格式]，請你將[問題回覆]找到適當的key值擷取並填入至以下完整Json格式，請勿改變與增加JSON格式。若敘述中沒有提到的資訊則將此問題欄位留空，若敘述回答忘記了或不知道則將此Json格式中的此問題欄位填入'未知'。你必須回答完整以下的Json格式且只回答Json格式，不要回答其餘無關事項。` +
-            `\n[Json格式]:\n ${JSON.stringify(requestData.incidentJson["車禍發生事故"])}` +
-            `\n[問題]:\n ${requestData.ccgCurrentQuestion}` +
-            `\n[問題回覆]:\n ${requestData.userDescription}` + 
-            `\n[Json]:`;
-
+            const { tidyPrompt } = require("./data/prompt")
             tidyMessage = [{ "role": "system", "content": tidyPrompt }]
             const gptResponse = await openai.createChatCompletion({
                 model: "gpt-3.5-turbo-1106",
@@ -133,15 +123,8 @@ exports.retrievalContent = async (req, res) => {
         }
 
         // + 提問模組
-        var questionKey = "你是一位交通諮詢代理人，使用溫柔的口氣表達對當事人發生的事感到惋惜，並且指示他'請點選下一步'。";
-        for (const key in responseData.incidentJson["車禍發生事故"]) {
-            if (!responseData.incidentJson["車禍發生事故"][key]) {
-                // responseData.ccgCurrentQuestion = `詢問一個有關${key}的問題`
-                questionKey = `你現在是一位交通諮詢專家，負責詢問一個有關'${key}'的問題給當事人。你的任務目標是問一個問題，而你要問的問題要依照以下[問題解釋]中'${key}'的解釋。你只能問一個問題，例如回覆'請問事故發生日期是何時?'。我方是指當事人。\n`
-                questionKey += `[問題解釋] : '${key}'的意思是'${questionExplain[key]}'`
-                break;
-            }
-        }
+        const { generateQuestionKey } = require("./data/prompt")
+        var questionKey = generateQuestionKey(responseData, questionExplain);
 
         const questionMessage = [ { "role": "system", "content": questionKey }, ]
         const gptResponse = await openai.createChatCompletion({
--- Server/controllers/data/prompt.js
+++ Server/controllers/data/prompt.js
@@ -1,6 +1,32 @@
 // - 事實生成模組 RefactorEvent
-exports.RefactorEventPrompt = `以下提供了一個以Json格式表示的車禍事實。請您依照此格式，以判決書的形式重述整個車禍的經過。請注意，您只需要描述在Json格式中提供的車禍相關事實，不應包含其他無關或未提供的資訊。`
+exports.RefactorEventPrompt = 
+`以下提供了一個以Json格式表示的車禍事實。請您依照此格式，以判決書的形式重述整個車禍的經過。請注意，您只需要描述在Json格式中提供的車禍相關事實，不應包含其他無關或未提供的資訊。`
+
+// - 初始模組
+exports.firstPrompt = 
+`你現在是一件交通諮詢的專家，現在有一件交通[事故敘述]，請你將資訊歸納成如下的[事件儲存Json格式]，如果沒有資料請保持欄位空白。\n` +
+`\n[事件儲存Json格式]:\n ${JSON.stringify(requestData.incidentJson["車禍發生事故"])}` +
+`\n[事故敘述]:\n ${requestData.userDescription}` +
+`\n[Json]:`;
 
 // - 提問模組
+exports.generateQuestionKey = (responseData, questionExplain) => {
+    var questionKey = "你是一位交通諮詢代理人，使用溫柔的口氣表達對當事人發生的事感到惋惜，並且指示他'請點選下一步'。";
+    for (const key in responseData.incidentJson["車禍發生事故"]) {
+        if (!responseData.incidentJson["車禍發生事故"][key]) {
+            questionKey = `你現在是一位交通諮詢專家，負責詢問一個有關'${key}'的問題給當事人。你的任務目標是問一個問題，而你要問的問題要依照以下[問題解釋]中'${key}'的解釋。你只能問一個問題，例如回覆'請問事故發生日期是何時?'。我方是指當事人。\n`
+            questionKey += `[問題解釋] : '${key}'的意思是'${questionExplain[key]}'`
+            break;
+        }
+    }
+    return questionKey;
+};
 
 // - 擷取模組
+exports.tidyPrompt = 
+`你是一位事件擷取機器人，現在有一[問題]與該[問題回覆]和一個[Json格式]，請你將[問題回覆]找到適當的key值擷取並填入至以下完整Json格式，請勿改變與增加JSON格式。若敘述中沒有提到的資訊則將此問題欄位留空，若敘述回答忘記了或不知道則將此Json格式中的此問題欄位填入'未知'。你必須回答完整以下的Json格式且只回答Json格式，不要回答其餘無關事項。` +
+`\n[Json格式]:\n ${JSON.stringify(requestData.incidentJson["車禍發生事故"])}` +
+`\n[問題]:\n ${requestData.ccgCurrentQuestion}` +
+`\n[問題回覆]:\n ${requestData.userDescription}` + 
+`\n[Json]:`;
+
